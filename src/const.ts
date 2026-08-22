44741cb9185d50a45e4206a9466c240a8a22f775
Caine72 <henrik.hedqvist@gmail.com>
2026-08-22 18:58:40 +0200

    Bump cards version to 0.3.0

--- src/const.ts
+++ src/const.ts
@@ -1,4 +1,4 @@
-export const CARD_VERSION = "0.2.0";
+export const CARD_VERSION = "0.3.0";
 export const DAILY_CARD_TYPE = "chores-manager-daily-card";
 export const OVERVIEW_CARD_TYPE = "chores-manager-overview-card";
 export const CORRECTION_CARD_TYPE = "chores-manager-correction-card";
